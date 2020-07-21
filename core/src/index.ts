b4164e8c846361a26c21b32493bb8b386257b232
Ivanq <imachug@yandex.ru>
2020-07-21 18:51:20 +0300

    Add an echo client/server, update peer list, enable DHT

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,22 +1,47 @@
 import {createNode} from "./node";
+import Multiaddr from "multiaddr";
 
 (async () => {
 	const node = await createNode();
 
 	node.on("peer:discovery", peer => {
-		console.log("Discovered %s", peer); // Log discovered peer
+		console.log("Discovered %s", peer.toB58String()); // Log discovered peer
 	});
 
-	node.on("peer:connect", peer => {
-		console.log("Connected to %s", peer); // Log connected peer
+	node.connectionManager.on("peer:connect", connection => {
+		console.log("Connected to %s", connection.remotePeer.toB58String());
 	});
+
+	node.handle("/chat/1.0.0", async ({stream}) => {
+		for await(const data of stream.source) {
+			console.log(data.toString());
+		}
+	});
+
 
 	await node.start();
-	console.log("libp2p has started");
-	const listenAddrs = node.transportManager.getAddrs();
-	console.log("libp2p is listening on the following addresses:", listenAddrs);
-	const advertiseAddrs = node.multiaddrs;
-	console.log("libp2p is advertising the following addresses:", advertiseAddrs);
+
+	console.log("Node multiaddrs:", node.multiaddrs.map(addr => {
+		return `${addr}/p2p/${node.peerId.toB58String()}`;
+	}));
+
+	if(process.argv.length >= 3) {
+		const ma = Multiaddr(process.argv[2]);
+
+		const {stream} = await node.dialProtocol(ma, "/chat/1.0.0");
+
+		function sleep(ms) {
+			return new Promise(resolve => setTimeout(resolve, ms));
+		}
+		async function* gen() {
+			for(let i = 1; ; i++) {
+				yield `Hello, world! ${i}\n`;
+				await sleep(1000);
+			}
+		}
+		stream.sink(gen());
+	}
+
 	//await node.stop();
 	//console.log("libp2p has stopped");
 })();
